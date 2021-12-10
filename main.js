138af4edb11672bec86f81b77cd2839fb6e3f89a
Hanna Marttila <hanna.marttila1@gmail.com>
2021-12-10 11:48:10 +0200

    JS viimeistelyä

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 //javascript
 let lomake = document.forms['formNewKaveri'];
 lomake.addEventListener('submit', uusiListaElementti)
-
+const kaveritListalla = new Array();
 function uusiListaElementti(event){
     
     event.preventDefault()
 
     // Haetaan elementille nimi input-kentästä
     let elementinNimi = document.querySelector('#main input[type="text"]').value;
+    kaveritListalla.push(elementinNimi);
+    console.log(kaveritListalla);
 
+    // huomautus
     if(elementinNimi.length < 1){
-        alert('Pitää antaa nimi');
+        alert('Kenttä on tyhjä, kirjoita nimi.');
         return;
     }
 
@@ -22,4 +25,12 @@ function uusiListaElementti(event){
 
     // Lisätään uusi li-elementti oikeaan paikkaan.
     document.querySelector('#kaveriList').appendChild(uusiElementti)
+
+    let kLen= kaveritListalla.length;
+    let text= "<ul>";
+    for (let i= 0; i < kLen; i++) {
+        text+= "<li>" +  kaveritListalla[i] + " </li>";
+    }
+    text +="</ul>";
+    document.getElementById('kaveriList').innerHTML= text;
 }
