e4c27191b5dd3d9c643373425406ff0770ceb32b
Hanna Marttila <hanna.marttila1@gmail.com>
2021-12-14 13:12:37 +0200

    projektin viimeistelyä

--- main.js
+++ main.js
@@ -2,35 +2,36 @@
 let lomake = document.forms['formNewKaveri'];
 lomake.addEventListener('submit', uusiListaElementti)
 const kaveritListalla = new Array();
+
+
 function uusiListaElementti(event){
     
     event.preventDefault()
 
     // Haetaan elementille nimi input-kentästä
     let elementinNimi = document.querySelector('#main input[type="text"]').value;
-    kaveritListalla.push(elementinNimi);
-    console.log(kaveritListalla);
 
-    // huomautus
-    if(elementinNimi.length < 1){
+     // huomautus
+     if(elementinNimi.length < 1){
         alert('Kenttä on tyhjä, kirjoita nimi.');
         return;
     }
-
-    // Luodaan uusi li-elementti ja sille sisältö.
-    let uusiElementti = document.createElement('li')
-    let uusiElementtiTeksti = document.createTextNode(elementinNimi)
-    uusiElementti.appendChild(uusiElementtiTeksti)
-    uusiElementti.className = 'kaveri-item';
-
-    // Lisätään uusi li-elementti oikeaan paikkaan.
-    document.querySelector('#kaveriList').appendChild(uusiElementti)
-
-    let kLen= kaveritListalla.length;
-    let text= "<ul>";
-    for (let i= 0; i < kLen; i++) {
-        text+= "<li>" +  kaveritListalla[i] + " </li>";
+    
+    if (kaveritListalla.length >=10) {
+        alert('Lista on täynnä!');
+        return;
+    } else {
+        kaveritListalla.push(elementinNimi);
+        console.log(kaveritListalla);
     }
-    text +="</ul>";
-    document.getElementById('kaveriList').innerHTML= text;
+        
+    let kLen= kaveritListalla.length;
+    let text= "";
+    for (let i= 0; i < kLen; i++) {
+        text+=  kaveritListalla[i] + "<br>";
+        document.getElementById('kaveriList').innerHTML= text;
+    }
+    
+    
 }
+
